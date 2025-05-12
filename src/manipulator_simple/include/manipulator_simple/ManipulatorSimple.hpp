c08dd8859d6fc503afc7e1aa4784210a8fb458b8
Marcin Walczyk <marcinwal9@gmail.com>
2025-05-12 16:08:03 +0200

    make manipulator manipulating again

--- src/manipulator_simple/include/manipulator_simple/ManipulatorSimple.hpp
+++ src/manipulator_simple/include/manipulator_simple/ManipulatorSimple.hpp
@@ -1,5 +1,7 @@
+#pragma once
+
 #include "rclcpp/rclcpp.hpp"
-#include "manipulator_simple/msg/manipulator_control.hpp"
+#include "rex_interfaces/msg/manipulator_mqtt_message.hpp"
 #include "rex_interfaces/msg/manipulator_control.hpp"
 extern "C"
 {
@@ -9,53 +11,17 @@ extern "C"
 class ManipulatorSimple
 {
 public:
-    ManipulatorSimple(rclcpp::Node::SharedPtr node) : mNode(node)
-    {
-        mSubMqtt = mNode->create_subscription<manipulator_simple::msg::ManipulatorControl>("/MQTT/ManipulatorControl", 100, std::bind(&ManipulatorSimple::handleInput, this, std::placeholders::_1));
-        mPubCan = mNode->create_publisher<rex_interfaces::msg::ManipulatorControl>("/CAN/TX/manipulator_ctl", 100);
-        
-        mLastCommandMessage = rex_interfaces::msg::ManipulatorControl();
-
-        mResenderTimer = mNode->create_timer(std::chrono::milliseconds(50), std::bind(&ManipulatorSimple::publishCommand, this));
-
-        mLastCommandMessage.axes[0].command_id = VESC_COMMAND_SET_DUTY;
-        mLastCommandMessage.axes[1].command_id = VESC_COMMAND_SET_DUTY;
-        mLastCommandMessage.axes[2].command_id = VESC_COMMAND_SET_DUTY;
-        mLastCommandMessage.axes[3].command_id = VESC_COMMAND_SET_DUTY;
-        mLastCommandMessage.axes[4].command_id = VESC_COMMAND_SET_DUTY;
-        mLastCommandMessage.axes[5].command_id = VESC_COMMAND_SET_DUTY;
-        mLastCommandMessage.gripper.command_id = VESC_COMMAND_SET_DUTY;
-
-        mLastCommandMessage.axes[0].set_value = 0.0f;
-        mLastCommandMessage.axes[1].set_value = 0.0f;
-        mLastCommandMessage.axes[2].set_value = 0.0f;
-        mLastCommandMessage.axes[3].set_value = 0.0f;
-        mLastCommandMessage.axes[4].set_value = 0.0f;
-        mLastCommandMessage.axes[5].set_value = 0.0f;
-        mLastCommandMessage.gripper.set_value = 0.0f;
-    }
+    ManipulatorSimple(rclcpp::Node::SharedPtr node);
 private:
-    void handleInput(manipulator_simple::msg::ManipulatorControl manipulatorControlMessage)
-    {
-        mLastCommandMessage.axes[0].set_value = manipulatorControlMessage.axis1;
-        mLastCommandMessage.axes[1].set_value = manipulatorControlMessage.axis2;
-        mLastCommandMessage.axes[2].set_value = manipulatorControlMessage.axis3;
-        mLastCommandMessage.axes[3].set_value = manipulatorControlMessage.axis4;
-        mLastCommandMessage.axes[4].set_value = manipulatorControlMessage.axis5;
-        mLastCommandMessage.axes[5].set_value = manipulatorControlMessage.axis6;
-        mLastCommandMessage.gripper.set_value = manipulatorControlMessage.gripper;
-    }
+    void handleInput(rex_interfaces::msg::ManipulatorMqttMessage manipulatorControlMessage);
 
-    void publishCommand()
-    {
-        mPubCan->publish(mLastCommandMessage);
-    }
+    void publishCommand();
 
     rclcpp::Node::SharedPtr mNode;
 
     rex_interfaces::msg::ManipulatorControl mLastCommandMessage;
 
-    rclcpp::Subscription<manipulator_simple::msg::ManipulatorControl>::SharedPtr mSubMqtt;
+    rclcpp::Subscription<rex_interfaces::msg::ManipulatorMqttMessage>::SharedPtr mSubMqtt;
     rclcpp::Publisher<rex_interfaces::msg::ManipulatorControl>::SharedPtr mPubCan;
 
     rclcpp::TimerBase::SharedPtr mResenderTimer;
